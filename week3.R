53aa0bd980b45de8116ae14f1dcbce8c033a2b28
Susmoy Pranto <65870018+susmoypranto1999@users.noreply.github.com>
2020-08-01 20:20:38 +0600

    Add files via upload

---
+++ week3.R
@@ -0,0 +1,26 @@
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(mean) inv <<- Inverse
+  getInverse <- function() inv
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+
+cachSolve <- function(x, ...) {
+  inv <- x$getInverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
+}
